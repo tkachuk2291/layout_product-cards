5636b25826a95bdd348a80fc0425d15698638007
Anton Tkachuk <tkacuk2291@gmail.com>
2024-10-23 20:25:50 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,58 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Card_with_data-qa_card.png",
+        "test": "../bitmaps_test/20241023-201335/Card_with_data-qa_card.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Card_with_data-qa_card.png",
+        "label": "Card with data-qa_card",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.6875,
+          "misMatchPercentage": "0.69",
+          "analysisTime": 8
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Link_with_data-qa_hover.png",
+        "test": "../bitmaps_test/20241023-201335/Link_with_data-qa_hover.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Link_with_data-qa_hover.png",
+        "label": "Link with data-qa_hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -2
+          },
+          "rawMisMatchPercentage": 1.475609756097561,
+          "misMatchPercentage": "1.48",
+          "analysisTime": 8
+        },
+        "diffImage": "../bitmaps_test/20241023-201335/failed_diff_Link_with_data-qa_hover.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
